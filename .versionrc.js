a4584d77d69d729a5b3465ed2d34a100ee6e98fe
Andrew Durber <aidurber@gmail.com>
2021-08-21 12:02:16 +0100

    build: add manifest updater

--- .versionrc.js
+++ .versionrc.js
@@ -1,9 +1,13 @@
-const tracker = {
+const versionUpdater = {
   filename: "versions.json",
   updater: require("./scripts/versions-updater"),
 };
+const manifestUpdater = {
+  filename: "manifest.json",
+  updater: require("./scripts/manifest-updater"),
+};
 
 module.exports = {
-  bumpFiles: [tracker],
-  packageFiles: [tracker],
+  bumpFiles: [versionUpdater, manifestUpdater],
+  packageFiles: [versionUpdater, manifestUpdater],
 };
