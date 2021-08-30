2476b52e72267c9e45acf14a6f3b982393dd8805
Andrew Durber <aidurber@gmail.com>
2021-08-30 17:23:37 +0000

    feat: support viewing parent tags

--- src/Tag.ts
+++ src/Tag.ts
@@ -19,20 +19,23 @@ export class Tag {
   }
 
   /**
-   * Get all subtags for a given tag
+   * Get all sub tags for a given tag
    */
   get subTags(): Tag[] {
     return this._text.split("/").reduce((acc, curr, index) => {
       const previous = index > 0 ? acc[index - 1] : null;
-
-      if (previous) {
-        return [...acc, new Tag(`${previous.raw}/${curr}`)];
-      }
-
-      return [...acc, new Tag(curr)];
+      const tagContent = previous ? `${previous.raw}/${curr}` : curr;
+      return [...acc, Tag.create(tagContent)];
     }, [] as Tag[]);
   }
 
+  /**
+   * Determine whether or not this tag has sub tags
+   */
+  get hasSubTags(): boolean {
+    // The original list will contain itself, check if it has subtags other than itself
+    return this.subTags.filter((t) => !t.equals(this)).length > 0;
+  }
   /**
    * Determine whether a clicked element was an Obsidian tag
    * @param target - Click target
@@ -46,6 +49,11 @@ export class Tag {
     );
   }
 
+  /**
+   * Build a complete tag from a partial tag
+   * @param target - Click target
+   * @returns
+   */
   static buildTagFromPartial(target: HTMLElement): Tag | null {
     const tagClass = Array.from(target.classList).find((cls) =>
       cls.startsWith("cm-tag")
@@ -66,9 +74,30 @@ export class Tag {
    * @param element - Clicked element
    * @returns
    */
-  static create(element: HTMLElement | Tag): Tag {
+  static create(element: HTMLElement | Tag | string): Tag {
     if (element instanceof Tag) return element;
-    const content = element.textContent.replace("#", "");
-    return new Tag(content);
+    const content: string =
+      typeof element === "string" ? element : element.textContent;
+    return new Tag(content.replace("#", ""));
+  }
+
+  /**
+   * Check if two tags are equal
+   * @param other - Other tag to compare to this
+   * @returns
+   */
+  equals = (other: Tag): boolean => {
+    return this.tag === other.tag;
+  };
+
+  /**
+   * Check if two tags are equal
+   *
+   * @param left - Left tag to compare
+   * @param right - Right tag to compare
+   * @returns
+   */
+  static equals(left: Tag, right: Tag) {
+    return left.equals(right);
   }
 }
--- src/TagDetailsModal.ts
+++ src/TagDetailsModal.ts
@@ -3,9 +3,17 @@ import { Tag } from "./Tag";
 import { getAllFilesMatchingTag } from "./utils/getAllFilesMatchingTag";
 import { createTextContent, createLink } from "./utils/render";
 
+function sortFiles(a: TFile, b: TFile): number {
+  if (a.name > b.name) return 1;
+  if (a.name < b.name) return -1;
+  return 0;
+}
+
 export class TagDetailsModal extends Modal {
+  private originalTag: Tag;
   constructor(app: App, private tag: Tag) {
     super(app);
+    this.originalTag = tag;
   }
 
   onOpen() {
@@ -16,11 +24,21 @@ export class TagDetailsModal extends Modal {
     this.contentEl.empty();
   }
 
-  private renderContent() {
-    const pages = getAllFilesMatchingTag(this.app, this.tag);
-    const fragment = document.createDocumentFragment();
-    fragment.appendChild(createTextContent("h2", `Pages with ${this.tag.tag}`));
+  onSelectTag = (tag: string) => {
+    this.tag = Tag.create(tag);
+    this.contentEl.empty();
+    this.renderContent();
+  };
 
+  private renderContent() {
+    const tag = this.tag;
+    const pages = getAllFilesMatchingTag(this.app, tag);
+    const fragment = document.createDocumentFragment();
+    fragment.appendChild(createTextContent("h2", `Pages with ${tag.tag}`));
+
+    if (this.originalTag.hasSubTags) {
+      this.renderSubTagScope(fragment);
+    }
     if (pages.size === 0) {
       this.renderEmptyContent(fragment);
     } else {
@@ -30,6 +48,22 @@ export class TagDetailsModal extends Modal {
     this.contentEl.appendChild(fragment);
   }
 
+  private renderSubTagScope = (fragment: DocumentFragment) => {
+    const subTags = this.originalTag.subTags.filter(
+      (st) => !st.equals(this.tag)
+    );
+    const select = fragment.createEl("select", { cls: "dropdown" });
+    select.style.marginBottom = "1em";
+    select.value = this.tag.tag;
+    select.onchange = (e) => {
+      this.onSelectTag((e.target as HTMLSelectElement).value);
+    };
+    select.createEl("option", { value: "", text: "Change tag scope" });
+    subTags.forEach((subTag) => {
+      select.createEl("option", { value: subTag.tag, text: subTag.tag });
+    });
+  };
+
   /**
    * Render the links portion of the DOM
    * @param fragment - Document fragment to add content to
@@ -37,11 +71,7 @@ export class TagDetailsModal extends Modal {
    */
   private renderLinks(fragment: DocumentFragment, pages: Set<TFile>) {
     const list = document.createElement("ol");
-    const sortedPages = Array.from(pages).sort((a, b) => {
-      if (a.name > b.name) return 1;
-      if (a.name < b.name) return -1;
-      return 0;
-    });
+    const sortedPages = Array.from(pages).sort(sortFiles);
     for (let page of sortedPages) {
       const el = document.createElement("li");
       const link = createLink(this.app, page, () => this.close());
